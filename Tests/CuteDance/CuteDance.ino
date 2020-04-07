5837973351d5239a379909ac7ab11fff2f6aced2
Gabriel <gbrljorge@gmail.com>
2020-04-07 12:43:27 +0200

    dance fixes

--- Tests/CuteDance/CuteDance.ino
+++ Tests/CuteDance/CuteDance.ino
@@ -8,10 +8,15 @@
 #define ENB  10  // Needs to be a PWM pin (this one is a PWN on both the UNO and the MEGA)
 #define SERVO_PIN 2
 
-#define ARMS_UP 50
-#define ARMS_DOWN 0
+#define ARMS_UP 120
+#define ARMS_DOWN 180
 
 Servo servo;
+
+void clamp(int* x, int min, int max) {
+  if(*x < min) *x = min;
+  if(*x > max) *x = max;
+}
 
 // Sets the speed of each motor (an integer between -255 and 255)
 void setSpeed(int leftSpeed, int rightSpeed) {
@@ -48,43 +53,33 @@ void setup() {
     pinMode(ENB, OUTPUT);
     servo.attach(SERVO_PIN);
 
-    // Moves forward slowly
-    setSpeed(50,50);
     servo.write(ARMS_DOWN);
-    delay(2000);
-    setSpeed(0,0);
-    delay(100);
-
-    // Turns 90 degrees
-    setSpeed(-255,255);
-    delay(50);
-    setSpeed(0,0);
-    delay(100)
+    delay(10000);
     
     // Arms movements
     for(int i = 0; i < 3; i++) {
         servo.write(ARMS_UP);
-        delay(100);
+        delay(200);
         servo.write(ARMS_DOWN);
-        delay(100);
+        delay(200);
     }
 
     // Turns
     servo.write(ARMS_UP);
     setSpeed(-255,255);
-    delay(1000);
+    delay(2000);
+    delay(140);
     servo.write(ARMS_DOWN);
     setSpeed(0,0);
-    delay(300);
+    delay(200);
 
     // Arms movements
-        for(int i = 0; i < 3; i++) {
+    for(int i = 0; i < 2; i++) {
         servo.write(ARMS_UP);
-        delay(100);
+        delay(200);
         servo.write(ARMS_DOWN);
-        delay(100);
+        delay(200);
     }
 }
 
-void loop() {
-}
+void loop() {}
